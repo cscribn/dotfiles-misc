6364374abd359ed6d35b1e7ae44035dad792df0b
Chad Scribner <chadbscribner@gmail.com>
2022-10-12 16:10:41 -0500

    Fixing config path

--- powershell-core/Microsoft.PowerShell_profile.ps1
+++ powershell-core/Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,8 @@
-if (!(Test-Path -Path $HOME\.oh-my-posh)) {
-    git clone https://github.com/cscribn/oh-my-posh.git  $HOME\.oh-my-posh
+if (!(Test-Path -Path $HOME\.config\oh-my-posh)) {
+    git clone https://github.com/cscribn/oh-my-posh.git  $HOME\.config\oh-my-posh
 }
 
-oh-my-posh init pwsh --config $HOME\.oh-my-posh\themes\powerlevel10k_ansi.omp.json | Invoke-Expression
+oh-my-posh init pwsh --config $HOME\.config\oh-my-posh\themes\powerlevel10k_ansi.omp.json | Invoke-Expression
 Import-Module posh-git
 Import-Module -Name Terminal-Icons
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
