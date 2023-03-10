528e317fa7449c38ec8f79da19ce49ab84941fee
Chad Scribner <chadbscribner@gmail.com>
2023-03-10 11:57:06 -0600

    Threading for a tiny bit of speedup

--- powershell-core/Microsoft.PowerShell_profile.ps1
+++ powershell-core/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,8 @@
+Function Run-Step([string] $Description, [ScriptBlock]$Script)
+{
+  & $Script
+}
+
 If (-Not (Test-Path -Path $HOME\.config\oh-my-posh)) {
   git clone https://github.com/cscribn/config-oh-my-posh.git  $HOME\.config\oh-my-posh
 }
@@ -6,18 +11,29 @@ If ($IsMacOS) {
   $Env:PATH += ":/opt/homebrew/bin"
 }
 
-oh-my-posh init pwsh --config $HOME\.config\oh-my-posh\themes\powerlevel10k_amped_modern.omp.json | Invoke-Expression
-Import-Module posh-git
-Import-Module PSReadLine
-Import-Module -Name Terminal-Icons
-Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-Set-PSReadLineOption -PredictionViewStyle ListView
-Set-PSReadLineOption -EditMode Windows
+Run-Step "oh-my-posh" {
+	oh-my-posh init pwsh --config $HOME\.config\oh-my-posh\themes\powerlevel10k_amped_modern.omp.json | Invoke-Expression
+}
 
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+Run-Step "posh-git" {
+  Import-Module posh-git
+}
 
-If (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
+Run-Step "PSReadline" {
+  Import-Module PSReadLine
+  Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+  Set-PSReadLineOption -PredictionViewStyle ListView
+  Set-PSReadLineOption -EditMode Windows
+}
+
+Run-Step "Terminal-Icons" {
+  Import-Module -Name Terminal-Icons
+}
+
+Run-Step "Chocolatey" {
+  $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+
+  If (Test-Path($ChocolateyProfile)) {
+    Import-Module "$ChocolateyProfile"
+  }
 }
