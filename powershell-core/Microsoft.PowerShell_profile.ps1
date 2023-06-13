45d10b72c41ebeb14201858556a4b3c0c352127a
Chad Scribner <chadbscribner@gmail.com>
2023-06-13 08:01:54 -0500

    Removing threading

--- powershell-core/Microsoft.PowerShell_profile.ps1
+++ powershell-core/Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,3 @@
-Function Invoke-Step([string] $Description, [ScriptBlock]$Script)
-{
-  & $Script
-}
-
 If (-Not (Test-Path -Path $HOME\.config\oh-my-posh)) {
   git clone https://github.com/cscribn/config-oh-my-posh.git  $HOME\.config\oh-my-posh
 }
@@ -11,28 +6,15 @@ If ($IsMacOS) {
   $Env:PATH += ":/opt/homebrew/bin"
 }
 
-Invoke-Step "oh-my-posh" {
-	oh-my-posh init pwsh --config $HOME\.config\oh-my-posh\themes\powerlevel10k_amped_modern.omp.json | Invoke-Expression
-}
+oh-my-posh init pwsh --config $HOME\.config\oh-my-posh\themes\powerlevel10k_amped_modern.omp.json | Invoke-Expression
 
-Invoke-Step "posh-git" {
-  Import-Module posh-git
-}
+Import-Module posh-git
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineOption -EditMode Windows
+Import-Module -Name Terminal-Icons
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 
-Invoke-Step "PSReadline" {
-  Import-Module PSReadLine
-  Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-  Set-PSReadLineOption -EditMode Windows
-}
-
-Invoke-Step "Terminal-Icons" {
-  Import-Module -Name Terminal-Icons
-}
-
-Invoke-Step "Chocolatey" {
-  $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-
-  If (Test-Path($ChocolateyProfile)) {
-    Import-Module "$ChocolateyProfile"
-  }
+If (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
 }
