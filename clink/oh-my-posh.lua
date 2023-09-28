03376a5929764c9684695ee5a4c354ee1b8f57f7
Chad Scribner <chadbscribner@gmail.com>
2023-09-27 23:02:59 -0500

    Moving to classic.

--- clink/oh-my-posh.lua
+++ clink/oh-my-posh.lua
@@ -1,1 +1,1 @@
-load(io.popen('oh-my-posh init cmd --config ~/.config/oh-my-posh/themes/powerlevel10k_amped_modern.omp.json'):read("*a"))()
+load(io.popen('oh-my-posh init cmd --config ~/.config/oh-my-posh/themes/powerlevel10k_amped_classic.omp.json'):read("*a"))()
