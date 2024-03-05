a76c5560825154bd6f32e54b0a1b0614f3742bcb
Chad Scribner <chadbscribner@gmail.com>
2024-03-05 14:49:10 -0600

    Fixing mac path

--- powershell-core/Microsoft.PowerShell_profile.ps1
+++ powershell-core/Microsoft.PowerShell_profile.ps1
@@ -4,6 +4,7 @@ If (-Not (Test-Path -Path $HOME\.config\oh-my-posh)) {
 
 If ($IsMacOS) {
   $Env:PATH += ":/opt/homebrew/bin"
+  $Env:PATH = "/opt/homebrew/bin:" + $Env:PATH
 }
 
 oh-my-posh init pwsh --config $HOME\.config\oh-my-posh\themes\powerlevel10k_amped_modern.omp.json | Invoke-Expression
