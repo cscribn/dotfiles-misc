4989b335003fbc7fc53ed1e56a334d2a2943c102
Chad Scribner <chadbscribner@gmail.com>
2024-12-03 17:45:56 -0600

    Making more concise

--- powershell-core/Microsoft.PowerShell_profile.ps1
+++ powershell-core/Microsoft.PowerShell_profile.ps1
@@ -2,23 +2,13 @@ If (-Not (Test-Path -Path $HOME\.config\oh-my-posh)) {
   git clone https://github.com/cscribn/dotfiles-oh-my-posh.git  $HOME\.config\oh-my-posh
 }
 
-If ($IsMacOS) {
-  $Env:PATH += ":/opt/homebrew/bin"
-  $Env:PATH = "/opt/homebrew/bin:" + $Env:PATH
-}
+If ($IsMacOS) { $Env:PATH = "/opt/homebrew/bin:" + $Env:PATH }
 
 oh-my-posh init pwsh --config $HOME\.config\oh-my-posh\themes\powerlevel10k_amped_modern.omp.json | Invoke-Expression
 
-If (-Not $IsLinux) {
-  Import-Module posh-git
-  Import-Module -Name Terminal-Icons
-}
+If (-Not $IsLinux) { Import-Module posh-git; Import-Module -Name Terminal-Icons }
 
-Import-Module PSReadLine
-Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-Set-PSReadLineOption -EditMode Windows
+Import-Module PSReadLine; Set-PSReadLineOption -EditMode Windows -PredictionSource HistoryAndPlugin
+
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-
-If (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
+If (Test-Path($ChocolateyProfile)) { Import-Module "$ChocolateyProfile" }
