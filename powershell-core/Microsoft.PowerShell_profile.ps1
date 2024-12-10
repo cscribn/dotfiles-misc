54413977ba0422f683aea89366a20e77d85cd654
Chad Scribner <chadbscribner@gmail.com>
2024-12-10 16:14:43 -0600

    Creating mise activate

--- powershell-core/Microsoft.PowerShell_profile.ps1
+++ powershell-core/Microsoft.PowerShell_profile.ps1
@@ -3,6 +3,7 @@ If (-Not (Test-Path -Path $HOME\.config\oh-my-posh)) {
 }
 
 If ($IsMacOS) { $Env:PATH = "/opt/homebrew/bin:" + $Env:PATH }
+If ($IsWindows -And (Get-Command mise -ErrorAction SilentlyContinue)) { $Env:Path += ";$env:LOCALAPPDATA\mise\shims" }
 
 oh-my-posh init pwsh --config $HOME\.config\oh-my-posh\themes\powerlevel10k_amped_modern.omp.json | Invoke-Expression
 
