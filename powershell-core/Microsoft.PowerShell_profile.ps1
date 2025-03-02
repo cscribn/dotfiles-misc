26051470ac5b25f3d36409b027766f15a547d1c9
Chad Scribner <chadbscribner@gmail.com>
2025-03-02 10:09:10 -0600

    Fixing oh-my-posh placement

--- powershell-core/Microsoft.PowerShell_profile.ps1
+++ powershell-core/Microsoft.PowerShell_profile.ps1
@@ -4,11 +4,11 @@ If (-Not (Test-Path -Path $HOME\.config\oh-my-posh)) {
 
 If ($IsMacOS) { $Env:PATH = "/opt/homebrew/bin:" + $Env:PATH }
 
+oh-my-posh init pwsh --config $HOME\.config\oh-my-posh\themes\powerlevel10k_amped_modern.omp.json | Invoke-Expression
+
 If (-Not $IsLinux) { Import-Module posh-git; Import-Module -Name Terminal-Icons }
 
 Import-Module PSReadLine; Set-PSReadLineOption -EditMode Windows -PredictionSource History
 
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 If (Test-Path($ChocolateyProfile)) { Import-Module "$ChocolateyProfile" }
-
-oh-my-posh init pwsh --config $HOME\.config\oh-my-posh\themes\powerlevel10k_amped_modern.omp.json | Invoke-Expression
