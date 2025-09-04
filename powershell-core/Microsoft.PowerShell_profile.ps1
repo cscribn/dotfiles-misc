ac275cf23c2e09d5fc2310df3e743f741b9435e9
Chad Scribner <chadbscribner@gmail.com>
2025-09-04 16:02:40 -0500

    Change home to ~

--- powershell-core/Microsoft.PowerShell_profile.ps1
+++ powershell-core/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,10 @@
-If (-Not (Test-Path -Path $HOME\.config\oh-my-posh)) {
-  git clone https://github.com/cscribn/dotfiles-oh-my-posh.git  $HOME\.config\oh-my-posh
+If (-Not (Test-Path -Path ~\.config\oh-my-posh)) {
+  git clone https://github.com/cscribn/dotfiles-oh-my-posh.git  ~\.config\oh-my-posh
 }
 
 If ($IsMacOS) { $Env:PATH = "/opt/homebrew/bin:" + $Env:PATH }
 
-oh-my-posh init pwsh --config $HOME\.config\oh-my-posh\themes\powerlevel10k_amped_modern.omp.json | Invoke-Expression
+oh-my-posh init pwsh --config ~\.config\oh-my-posh\themes\powerlevel10k_amped_modern.omp.json | Invoke-Expression
 
 If (-Not $IsLinux) {
   If (-Not (Get-Module -Name posh-git)) { Import-Module posh-git }
