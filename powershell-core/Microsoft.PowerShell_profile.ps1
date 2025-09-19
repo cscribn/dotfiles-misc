b38bc7dd57569db11f7c10fc451ac2ff57fdf80c
Chad Scribner <chadbscribner@gmail.com>
2025-09-19 10:29:56 -0500

    Using OS separators.

--- powershell-core/Microsoft.PowerShell_profile.ps1
+++ powershell-core/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,13 @@
-If (-Not (Test-Path -Path ~\.config\oh-my-posh)) {
+$Env:POWERSHELL_UPDATECHECK = 'Off'
+$Sep = [IO.Path]::DirectorySeparatorChar
+
+If (-Not (Test-Path -Path ~${Sep}.config${Sep}oh-my-posh)) {
   git clone https://github.com/cscribn/dotfiles-oh-my-posh.git  ~\.config\oh-my-posh
 }
 
 If ($IsMacOS) { $Env:PATH = "/opt/homebrew/bin:" + $Env:PATH }
 
-oh-my-posh init pwsh --config ~\.config\oh-my-posh\themes\powerlevel10k_amped_modern.omp.json | Invoke-Expression
+oh-my-posh init pwsh --config ~${Sep}.config${Sep}oh-my-posh${Sep}themes${Sep}powerlevel10k_amped_modern.omp.json | Invoke-Expression
 
 If (-Not $IsLinux) {
   If (-Not (Get-Module -Name posh-git)) { Import-Module posh-git }
