e250d5accb75ad73f3bf5555944acd603e7668c6
Chad Scribner <chadbscribner@gmail.com>
2026-03-22 19:10:02 -0500

    Add env variables for trailing/leading symbols.

--- clink/oh-my-posh.lua
+++ clink/oh-my-posh.lua
@@ -1,1 +1,3 @@
-load(io.popen('oh-my-posh init cmd --config ~/.config/oh-my-posh/themes/powerlevel10k_amped_classic.omp.json'):read("*a"))()
+os.setenv("LEADING_SYMBOL", "(")
+os.setenv("TRAILING_SYMBOL", ")")
+load(io.popen('oh-my-posh init cmd --config ~/.config/oh-my-posh/themes/powerlevel10k_amped_rainbow.omp.json'):read("*a"))()
--- powershell-core/Microsoft.PowerShell_profile.ps1
+++ powershell-core/Microsoft.PowerShell_profile.ps1
@@ -11,7 +11,9 @@ If (-Not (Test-Path -Path ~${Sep}.config${Sep}oh-my-posh)) {
 
 If ($IsMacOS) { $Env:PATH = "/opt/homebrew/bin:" + $Env:PATH }
 
-oh-my-posh init pwsh --config ~${Sep}.config${Sep}oh-my-posh${Sep}themes${Sep}powerlevel10k_amped_modern.omp.json | Invoke-Expression
+$Env:LEADING_SYMBOL = '('
+$Env:TRAILING_SYMBOL = ')'
+oh-my-posh init pwsh --config ~${Sep}.config${Sep}oh-my-posh${Sep}themes${Sep}powerlevel10k_amped_rainbow.omp.json | Invoke-Expression
 
 If (-Not $IsLinux) {
   If (-Not (Get-Module -Name posh-git)) { Import-Module posh-git }
