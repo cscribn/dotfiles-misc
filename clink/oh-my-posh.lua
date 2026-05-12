7cb1cb839691efc5d4efad424bc4032daf493c2c
Chad Scribner <chadbscribner@gmail.com>
2026-05-12 14:18:45 -0500

    Update symbols.

--- clink/oh-my-posh.lua
+++ clink/oh-my-posh.lua
@@ -1,3 +1,3 @@
-os.setenv("LEADING_SYMBOL", "(")
-os.setenv("TRAILING_SYMBOL", ")")
+os.setenv("LEADING_SYMBOL", "/")
+os.setenv("TRAILING_SYMBOL", "/")
 load(io.popen('oh-my-posh init cmd --config ~/.config/oh-my-posh/themes/powerlevel10k_amped_rainbow.omp.json'):read("*a"))()
